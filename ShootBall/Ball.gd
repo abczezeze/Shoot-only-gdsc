f134ecd8a1e8ae1d7eef3e814f5ca6255cd805c9
ABCzezeze <archueyouler@gmail.com>
2022-02-04 20:54:45 +0700

    CircleHeart4Shoot

--- ShootBall/Ball.gd
+++ ShootBall/Ball.gd
@@ -2,11 +2,13 @@ extends RigidBody
 
 const KILL_TIMER = 10
 var timer = 0
+var rand_x = rand_range(-10,10)
+var rand_y = rand_range(-10,10)
+var rand_z = rand_range(-10,10)
 
 func _physics_process(delta):
 #	apply_central_impulse(Vector3(0,0,-10))
-#	apply_impulse(Vector3(rand_range(-10,10),rand_range(-10,10),rand_range(-10,10)),Vector3(0,0,-10))
-	apply_impulse(Vector3(rand_range(-10,10),rand_range(-10,10),rand_range(-10,10)), global_transform.origin*5)
+	apply_impulse(Vector3(), global_transform.origin*5)
 	timer += delta
 	if timer >= KILL_TIMER:
 		queue_free()
