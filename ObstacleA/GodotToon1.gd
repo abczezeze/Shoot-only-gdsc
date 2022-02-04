94229b3f787b9611042d77052179870576ce8e81
ABCzezeze <archueyouler@gmail.com>
2022-02-04 20:52:25 +0700

    GodotToon FollowPath

---
+++ ObstacleA/GodotToon1.gd
@@ -0,0 +1,21 @@
+extends RigidBody
+
+const BASE_BULLET_BOOST = 0.1;
+var qf = 0
+var ro_y_speed = 10
+	
+func _physics_process(delta):
+	$Plane.rotate_y(ro_y_speed * delta)
+	var direction_vect = global_transform.basis.z.normalized() * BASE_BULLET_BOOST;
+
+	qf += delta
+	if qf >= 5:
+		queue_free()
+
+#func _process(delta):
+#	$Plane.rotate_y(delta)
+
+func _on_GodotToon1_body_entered(body):
+	if body is RigidBody:
+		self.gravity_scale = -1
+
